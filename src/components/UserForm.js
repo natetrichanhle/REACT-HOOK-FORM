0072d49cb7afcd64b010b10a36765bc69f01a5a4
Nate Trichanh <natetrichanh@gmail.com>
2022-01-08 20:39:30 -0800

    initial commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from  'react';
+    
+    
+const UserForm = (props) => {
+    const [firstName, setfirstName] = useState("");
+    const [lastName, setlastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setconfirmPassword] = useState("");
+    
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstName, lastName, email, password, confirmPassword };
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <div>
+            <form onSubmit={ createUser }>
+                <div>
+                    <label>First Name: </label> 
+                    <input type="text" onChange={ (e) => setfirstName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Last Name: </label> 
+                    <input type="text" onChange={ (e) => setlastName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Email Address: </label> 
+                    <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                </div>
+                <div>
+                    <label>Confirm Password: </label>
+                    <input type="text" onChange={ (e) => setconfirmPassword(e.target.value) } />
+                </div>
+                <input type="submit" value="Create User" />
+            </form>
+            <h1>Your Form Data</h1>
+            <p>First Name: {firstName}</p>
+            <p>Last Name: {lastName}</p>
+            <p>Email Address: {email}</p>
+            <p>Password: {password}</p>
+            <p>Confirm Password: {confirmPassword}</p>
+        </div>
+    );
+};
+    
+export default UserForm;
